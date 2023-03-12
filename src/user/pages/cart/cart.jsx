53550a80ef96fc70b879c3696b30a9bf1d6b08e6
van <nguyenthevann6@gmail.com>
2023-03-12 22:18:52 +0700

    abc

--- src/user/pages/cart/cart.jsx
+++ src/user/pages/cart/cart.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect,memo  } from "react";
 import { useAuth } from "../../contexts/AuthContext";
 import axios from "axios";
 import "../../assets/shop/cart.css";
